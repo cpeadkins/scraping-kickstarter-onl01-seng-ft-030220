62930355d46f511c10479222d710be4d1341bb29
Charles Adkins <cpeadkins@gmail.com>
2020-03-31 17:24:43 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,6 +5,8 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
  
+ binding.pry
+ 
   projects = {}
  
   kickstarter.css("li.project.grid_4").each do |project|
@@ -16,7 +18,6 @@ def create_project_hash
       :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
     }
     
-      binding.pry
   end
  
   projects
